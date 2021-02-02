3734085cafc35c05f81edf88cbe01e55ec32b91b
Verónica Velázquez <veronicavelazquezcalleja@gmail.com>
2021-02-02 12:22:59 +0100

    img and grid template

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FileSystem</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="src/css/main.css">
+</head>
+<body class="main-container">
+    <header class="wrapper-header">
+        <h4>Here it will be the path root</h4>
+    </header>
+    <aside class="wrapper-folders">
+        <div class="folders-fixed">
+            <h5>root</h5>
+            <h5>trash</h5>
+        </div>
+    </aside>
+    <div class="wrapper-roots">
+        <p>aqui van las rutas</p>
+    </div>
+</body>
+</html>
