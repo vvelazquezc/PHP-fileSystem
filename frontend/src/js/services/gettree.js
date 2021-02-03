7692d657cde5832b980ea02169a24859fbf2ecd1
Verónica Velázquez <veronicavelazquezcalleja@gmail.com>
2021-02-03 15:27:34 +0100

    get absolutePath

---
+++ frontend/src/js/services/gettree.js
@@ -0,0 +1,25 @@
+import { $wrapperRoot } from "../components/folder.js"
+
+const dir = 'http://192.168.64.2/php/PHP-fileSystem/backend/folder/gettree.php'
+const $content = document.querySelector('.folders-path')
+
+function getAbsolutePath(dir) {
+    fetch(dir, {
+        method: 'get',
+        // may be some code of fetching comes here
+    }).then(function(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response.text()
+        }
+        throw new Error(response.statusText)
+    })
+    .then(function(response) {
+        const listOfResults = JSON.parse(response)
+        $wrapperRoot.render($content, listOfResults)
+
+    })
+}
+getAbsolutePath(dir)
+
+
+
