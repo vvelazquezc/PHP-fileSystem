f5cf847334f02773ea915e894d46c2593954cdf3
Leyber91 <leyber.91.2@gmail.com>
2021-02-08 18:25:54 +0100

    uploading file initialized

--- backend/file/upload.php
+++ backend/file/upload.php
@@ -1,8 +1,16 @@
+TODO: 
+we need to receive, a Parent absolute path like a parameter:
+ -  /folder/create?parentAbsolutePath=/
+
+Then, we need to make de fetch to php server.
+ */
+
 <?php
-$target_dir = "uploads/";
+$target_dir = "http://localhost/LeyberProject/PHP-fileSystem/root/"; //Here we need to add the specific path
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -17,8 +25,20 @@ if(isset($_POST["submit"])) {
 
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-  }
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
 
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
 ?>
