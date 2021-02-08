c728f41835143a40df146789b602fcf2a9bb6b7e
Leyber91 <leyber.91.2@gmail.com>
2021-02-08 18:32:02 +0100

    quick

--- backend/file/upload.php
+++ backend/file/upload.php
@@ -1,27 +1,10 @@
-TODO: 
-we need to receive, a Parent absolute path like a parameter:
- -  /folder/create?parentAbsolutePath=/
 
-Then, we need to make de fetch to php server.
- */
-
-<?php
-$target_dir = "http://localhost/LeyberProject/PHP-fileSystem/root/"; //Here we need to add the specific path
+ <?php
+$target_dir = "http://localhost/LeyberProject/PHP-fileSystem/root/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
 
 // Check if file already exists
 if (file_exists($target_file)) {
@@ -29,6 +12,11 @@ if (file_exists($target_file)) {
   $uploadOk = 0;
 }
 
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
