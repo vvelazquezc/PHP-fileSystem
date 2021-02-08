5ce93535b7bbb331c6510f697ab6923b16339643
Leyber91 <leyber.91.2@gmail.com>
2021-02-08 16:32:33 +0100

    pre checkout

--- backend/file/upload.php
+++ backend/file/upload.php
@@ -1,6 +1,24 @@
-TODO: 
-we need to receive, a Parent absolute path like a parameter:
- -  /folder/create?parentAbsolutePath=/
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
 
-Then, we need to make de fetch to php server.
- */
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+
+?>
