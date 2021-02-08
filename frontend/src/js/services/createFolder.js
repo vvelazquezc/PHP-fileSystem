e9cff60511595d49272b062c213df4751e51a070
Leyber91 <leyber.91.2@gmail.com>
2021-02-08 12:40:15 +0100

    pre pull

--- frontend/src/js/services/createFolder.js
+++ frontend/src/js/services/createFolder.js
@@ -1,31 +1,15 @@
-const endpointUrl = 'http://192.168.64.2/php/PHP-fileSystem/backend/folder/create.php'
+const endpointUrl = 'http://localhost/LeyberProject/PHP-fileSystem/backend/folder/create.php'
 
 function createFolder(folderName, parentAbsolutePath) {
     const folderAbsolutePath = `${parentAbsolutePath}/${folderName}`
 
-<<<<<<< HEAD
-    fetch(`${endpointUrl}?folder=${folderAbsolutePath}`)
-=======
     return fetch(`${endpointUrl}?folder=${folderAbsolutePath}`)
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
         .then(function(response) {
             if (response.status >= 200 && response.status < 300) {
                 return response.json()
             }
             throw new Error(response.statusText)
         })
-<<<<<<< HEAD
-        .then(function({ isCreated }) {
-            if (!isCreated) {
-                throw new Error('folder exists')
-            }
-            renderFolder(parentAbsolutePath)
-        })
-        .catch(function(error) {
-            console.warn(error.message)
-        })
-=======
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
 }
 
 export { createFolder }
--- frontend/src/js/services/info.js
+++ frontend/src/js/services/info.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import { $wrapperInfo } from "../components/info.js"
-
-const dir = 'http://localhost/LeyberProject/PHP-fileSystem/backend/folder/read.php'
-=======
-const endpointUrl = 'http://192.168.64.2/php/PHP-fileSystem/backend/folder/read.php'
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
+const endpointUrl = 'http://localhost/LeyberProject/PHP-fileSystem/backend/folder/read.php'
 const $content = document.querySelector('.folders-info')
 
 function getInfoFolder(folder) {
