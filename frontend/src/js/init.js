cc143e58303da31870b4f42bf72e600d830c6885
Leyber91 <leyber.91.2@gmail.com>
2021-02-08 17:00:53 +0100

    premerge

--- frontend/src/js/init.js
+++ frontend/src/js/init.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
-import { getAbsolutePath } from "./services/gettree.js";
-import { getInfo } from "./services/info.js";
-
- getAbsolutePath(dir)
- getInfo(file, dir)
-<<<<<<< HEAD
-=======
-=======
-import { folderColumnComponent } from './components/folder.js'
->>>>>>> 152b9064c4b49552fbf6bf163ca7899937d1d636
+import { folderColumnComponent } from "./components/folder.js"
 
 folderColumnComponent.renderOnLeft('/')
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
--- frontend/src/js/services/gettree.js
+++ frontend/src/js/services/gettree.js
@@ -1,17 +1,7 @@
 const endpointUrl = 'http://192.168.64.2/php/PHP-fileSystem/backend/folder/getTree.php'
 
-<<<<<<< HEAD
-const endpointUrl = 'http://localhost/LeyberProject/PHP-fileSystem/backend/folder/gettree.php'
-const $content = document.querySelector('.folders-path')
-const $folders = document.querySelector('.folders-tree')
-const $secondTree = document.querySelectorAll('.folders-path')[1]
-
-function renderFolder(folderAbsolutePath, secondWrapper = false, treeMenu = false) {
-    fetch(`${endpointUrl}?folder=${folderAbsolutePath}`)
-=======
 function getTree(folderAbsolutePath) {
     return fetch(`${endpointUrl}?folder=${folderAbsolutePath}`)
->>>>>>> 9fd18439a41dd783ce5d668543bc232d27c184ee
         .then(function(response) {
             if (response.status >= 200 && response.status < 300) {
                 return response.json()
